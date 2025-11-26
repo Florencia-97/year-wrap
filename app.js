f1f9d75f1231f552c86a5066a85d537ca2589871
Florencia <florrr1997@gmail.com>
2025-11-26 14:20:13 -0300

    fixis styes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const wrapper = document.getElementById('wrapper');
 let slides = [];
 let current = 0;
+let started = false;
 
 async function loadWrapped() {
   try {
@@ -8,7 +9,6 @@ async function loadWrapped() {
     const user_to_load = params.get('user');
     const res = await fetch('data.json');
     const data = await res.json();
-    console.log(user_to_load)
     buildSlides(data['socies'][user_to_load], data['extra']);
   } catch (e) {
     console.error('Error loading JSON:', e);
@@ -21,36 +21,36 @@ function buildSlides(data, extra) {
   // 1. Intro Slide
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #9b5de5, #f15bb5)`,
-    `<h1>Hola ${data.name} !</h1><p>Bienvenido a tu wrap de academia 2025</p><button id="startBtn">Start 🎶</button>`
+    `<h1>¡Hola ${data.name}!</h1><p>Tu wrap de academia 2025</p><button id="startBtn">Comenzar 📚</button>`
   ));
 
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #9b5de5, #f15bb5)`,
-    `<h1>Sabias que este año hubo ${extra.cantidad_de_capacitaciones} capitaciones y la más popular fue ${extra.capacitacion_mas_popular}?</h1>`
+    `<h1>Sabias que este año hubo <span>${extra.cantidad_de_capacitaciones} capitaciones</span> y la más popular fue <span>${extra.capacitacion_mas_popular}</span>?</h1>`
   ));
 
   const number_of_assists_percentage = data.mas_capacitaciones_que_X;
   
   let assist_message = "";
   if (data.asististe_a_X_capacitaciones >= 60) {
-    assist_message = "Sos un capo"
+    assist_message = "sos un capo"
   } else if (data.asististe_a_X_capacitaciones >= 30) {
-    assist_message = "Sólido"
+    assist_message = "muy sólido"
   } else {
-    assist_message = "Nos gustaria verte más seguido!"
+    assist_message = "nos gustaria verte más seguido!"
   }
 
   // 1. Asistencia
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #fee440, #f15bb5)`,
-    `<h2>Asististe a</h2><p class="number_of_events">${data.asististe_a_X_capacitaciones} capacitaciones. ${assist_message}</p></br>
-    <p>Eso es más que el ${number_of_assists_percentage} de los socies</p>`,
+    `<h2>Asististe a</h2><p class="number_of_events">${data.asististe_a_X_capacitaciones} capacitaciones, ${assist_message}</p></br>
+    <p class=bottom_notation>¡Eso es más que el ${number_of_assists_percentage} de los socies!</p>`,
   ));
 
   // 4. Mes más concurrido
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #1db954, #00bbf9)`,
-    `<h2>Ya viene diciembre y estamos abriendo los regalos, sabias que el mes en el que más te capacitaste fue en </h2><p class="number_of_events">${data.mes_que_mas_capacitaste} ?</p>`
+    `<h2>Ya viene diciembre y estamos abriendo los regalos. ¿Sabias que el mes en el que más te capacitaste fue en </h2><p class="number_of_events">${data.mes_que_mas_capacitaste} ?</p>`
   ));
 
 
@@ -63,28 +63,29 @@ function buildSlides(data, extra) {
   // 1. Categoria y formato
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #fee440, #f15bb5)`,
-    `<h2> Tu categoria más popular fue</h2><p class="number_of_events">${data.tu_categoria_mas_popular_fue}</p></br>
-    <p>El formato que parece que te gusta más es ${data.el_formato_que_mas_asististe}.</p>`,
+    `<h2> Tu categoria más popular fue</h2><p class="number_of_events">"${data.tu_categoria_mas_popular_fue}"</p></br>
+    <p class=bottom_notation>El formato que parece que te gusta más es ${data.el_formato_que_mas_asististe}.</p>`,
   ));
 
   // 1. Amigo
     wrapper.appendChild(createSlide(
       `linear-gradient(135deg, #fee440, #f15bb5)`,
       `<h2>Capacitarse solo es aburrido! Tu media naranja de academia es</h2><p class="number_of_events">${data.amiguito_con_que_mas_asististe} 🫂</p></br>
-      <p>Se vieron ${data.amiguito_con_que_mas_asististe_cantidad} veces!</p>`,
+      <p class=bottom_notation>Se vieron ${data.amiguito_con_que_mas_asististe_cantidad} veces!</p>`,
   ));
 
 
   // 5. Ghandi
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #9b5de5, #fee440)`,
-    `<h2>"Vive como si fueras a morir mañana, aprende como si fueras a vivir siempre"</h2><p>Ghandi</p>`
+    `<h2>"Vive como si fueras a morir mañana, aprende como si fueras a vivir siempre"</h2>
+    <p class=bottom_notation>Ghandi</p>`
   ));
 
   // 5. Outro
   wrapper.appendChild(createSlide(
     `linear-gradient(135deg, #9b5de5, #fee440)`,
-    `<h2>Nos vemos el próximo año! Gracias por todo 💚</h2><button id="restartBtn">Replay</button>`
+    `<h2>¡Nos vemos el próximo año! Gracias por todo 💚</h2><button id="restartBtn">Replay</button>`
   ));
 
   slides = document.querySelectorAll('.slide');
@@ -100,9 +101,14 @@ function createSlide(bg, html) {
 }
 
 function addEventListeners() {
-  document.getElementById('startBtn').addEventListener('click', nextSlide);
+  document.getElementById('startBtn').addEventListener('click', start);
   document.getElementById('restartBtn').addEventListener('click', restart);
   slides[0].classList.add('active');
+}
+
+function start() {
+  started = true;
+  nextSlide();
 }
 
 function nextSlide() {
@@ -117,9 +123,9 @@ function restart() {
   slides[current].classList.add('active');
 }
 
-// Auto advance every 5s
+// Auto advance every 7s
 setInterval(() => {
-  if (slides.length && current < slides.length - 1) nextSlide();
-}, 8000);
+  if (slides.length && current < slides.length - 1 && started) nextSlide();
+}, 7000);
 
 loadWrapped();
